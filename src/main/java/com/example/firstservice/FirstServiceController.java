56cafc07536322e5646a9392fecf2cff9678432e
yujinlee <now8567@hanmail.net>
2023-05-15 23:27:13 +0900

    header log 추가

--- src/main/java/com/example/firstservice/FirstServiceController.java
+++ src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,16 +1,28 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// http://localhost:8081/welcome # zuul
+// http://localhost:8081/first-service/welcome # spring cloud gateway
 @RestController
-@RequestMapping("/")
+//@RequestMapping("/")
+@RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
 
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the First service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request")String header){
+        log.info(header);
+        return "Hello World in First Service.";
     }
 }
